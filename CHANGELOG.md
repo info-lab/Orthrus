d9f31c2254a182e017c6d2d33b52cccfcb3cae00
bconstanzo <bru.constanzo@gmail.com>
2014-10-27 19:15:35 -0300

    0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+Version 0.2:
+------------
+* Barely works, it can extract a few .doc files but its messy.
+* Lots of prints lying around the code for debugging.
+
 Version 0.1:
 ------------
 * Skeleton of the carver
--- orthrus.py
+++ orthrus.py
@@ -15,16 +15,18 @@
 # coding=utf-8
 
 import argparse
+import cStringIO
 import datetime
 import FileValidators
+import re
 import os
 
 
 # A few constants:
 DEBUG_BENCHMARK = True
-CONST_BUILD = 1
-CONST_VER = "0.1"
-CONST_VERSTRING = "Version %s.%s" % (CONST_VER, CONST_BUILD)
+CONST_BUILD = 5
+CONST_VER = "0.2.1"
+CONST_VERSTRING = "Version %s build %s" % (CONST_VER, CONST_BUILD)
 CONST_YEARS = "2014"
 CONST_BANNER = """
 Orthrus carver
@@ -33,17 +35,99 @@ InFo-Lab prototype %s
 %s
 """ % (CONST_YEARS, CONST_VERSTRING)
 
+KILO = 1024
+MEGA = 1024 * KILO
+CONST_BLOCKSIZE = 10 * MEGA
+CONST_FILESIZE = 5 * MEGA
+CONST_SECTORSIZE = 512
+
 # And now some variables:
-validators = {
-    '.jpg': FileValidators.JPGValidator(),
-    '.png': FileValidators.PNGValidator(),
-    '.doc': FileValidators.MSOLEValidator(),
-    '.gif': FileValidators.GIFValidator(),
-}
+headers_list = [
+    #'\xff\xd8\xff',
+    '\x89\x50\x4e\x47\x0d\x0a\x1a\x0a',
+    '\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1',
+    #'GIF8',
+]
 
 
 def Carve(args):
-    pass
+    """
+    Performs bifragment gap carving on the files referenced by args.
+
+    :param args: dictionary with the arguments from the command line. Most important are the input
+    file and the output directory.
+    :return:
+    """
+    blocksize = CONST_BLOCKSIZE
+    filesize = CONST_FILESIZE
+    sectorsize = CONST_SECTORSIZE
+    headers = map(re.escape, headers_list)
+    rex_heads = re.compile("|".join(headers))
+    validators = {
+        '\xff\xd8\xff': FileValidators.JPGValidator(),
+        '\x89\x50\x4e\x47\x0d\x0a\x1a\x0a': FileValidators.PNGValidator(),
+        '\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1': FileValidators.MSOLEValidator(),
+        'GIF8': FileValidators.GIFValidator(),
+    }
+    extensions = {
+        '\xff\xd8\xff': ".jpg",
+        '\x89\x50\x4e\x47\x0d\x0a\x1a\x0a': ".png",
+        '\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1': ".doc",
+        'GIF8': ".gif",
+    }
+
+    image = open(args.ipath, "rb")
+    os.mkdir(args.opath)
+    ostring = args.opath + os.path.sep + "%08d%s"
+    ext_number = 1
+    block = image.read(blocksize)
+    readbytes = 0
+    while block:
+        readbytes = float(len(block)) / MEGA
+        print "-> %0.2f MB read" % readbytes
+        newblock = image.read(blocksize)
+        bigblock = block + newblock
+        match_results = rex_heads.finditer(block)
+        for match in match_results:
+            offset = match.start()
+            head = match.group()
+            val = validators[head]
+            data = bigblock[offset: offset + filesize]
+            obj = cStringIO.StringIO(data)
+            valid = val.Validate(obj)
+            if valid:
+                extract = True
+            else:
+                extract = False
+                lvb = val.GetStatus()[2]  # last valid byte
+                gap_start = (lvb / sectorsize) + 1
+                gap_end = (filesize / sectorsize) - 1
+                gap_size_start = 1
+                print "  file not valid, trying gaps..."
+                for gap_pos in xrange(gap_start, gap_end):
+                    print "\r    gaps starting from %d..." % (gap_pos),
+                    gap_size_end = gap_end - gap_pos
+                    for gap_size in xrange(gap_size_start, gap_size_end):
+                        pos1 = gap_pos * sectorsize
+                        pos2 = (gap_pos + gap_size) * sectorsize
+                        newdata = data[:pos1] + data[pos2:]
+                        new_obj = cStringIO.StringIO(newdata)
+                        if val.Validate(new_obj):
+                            extract = True
+                            data = newdata
+                            break
+                    if extract:
+                        extension = extensions[head]
+                        ext_size = val.GetStatus()[2]
+                        fo = open(ostring % (ext_number, extension), "wb")
+                        fo.write(data[:ext_size])
+                        fo.close()
+                        ext_number += 1
+                        break
+            if extract:
+                pass
+        block = newblock
+    image.close()
 
 
 def ArgParse():
@@ -76,7 +160,7 @@ def main():
         Carve(args)
     else:
         print "ipath argument must be a valid file!"
-        print "opath argument must be a non-existen directory!"
+        print "opath argument must be a non-existent directory!"
     dt = datetime.datetime.now() - t1
     if DEBUG_BENCHMARK:
         print "\nTime taken: %s" % dt
